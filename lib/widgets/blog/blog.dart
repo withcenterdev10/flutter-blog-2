191e50706939ea4e53bfe8f0befa65e67b7016e3
John Suyang <johnsuyang@gmail.com>
2026-02-04 15:57:06 +0800

    wrap blog content with card widget

--- lib/widgets/blog/blog.dart
+++ lib/widgets/blog/blog.dart
@@ -1,55 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog_2/models/blog_model.dart';
+import 'package:flutter_blog_2/providers/blog_providers.dart';
+import 'package:flutter_blog_2/screens/view_blog_screen.dart';
 import 'package:flutter_blog_2/utils.dart';
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 
 class Blog extends StatelessWidget {
   const Blog({super.key, required this.blog});
 
   final BlogModel blog;
 
+  void handleSelectBlog(BuildContext context, String id) async {
+    try {
+      ViewBlogScreen.go(context, blog.id!);
+      await context.read<BlogProvider>().getBlog(id);
+    } catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
+    return Card(
+      margin: EdgeInsets.all(12),
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          handleSelectBlog(context, blog.id!);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              blog.user?.imageUrl != null
-                  ? CircleAvatar(
-                      radius: 25,
-                      backgroundImage: NetworkImage(blog.user!.imageUrl!),
-                    )
-                  : CircleAvatar(
-                      radius: 25,
-                      child: Text(
-                        blog.user!.displayName!.substring(0, 2).toUpperCase(),
-                      ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  blog.user?.imageUrl != null
+                      ? CircleAvatar(
+                          radius: 25,
+                          backgroundImage: NetworkImage(blog.user!.imageUrl!),
+                        )
+                      : CircleAvatar(
+                          radius: 25,
+                          child: Text(
+                            blog.user!.displayName!
+                                .substring(0, 2)
+                                .toUpperCase(),
+                          ),
+                        ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 10),
+                        Text(
+                          truncateText(blog.title!),
+                          maxLines: 1,
+                          softWrap: false,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
                     ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 10),
-                    Text(
-                      truncateText(blog.title!),
-                      maxLines: 1,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(vertical: 4),
+                child: const Divider(),
+              ),
+              Text(blog.blog!),
             ],
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            child: Text(blog.blog!),
-          ),
-        ],
+        ),
       ),
     );
   }
