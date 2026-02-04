bd410978f65a6d5072c84591faf0a5d5079ac03a
John Suyang <johnsuyang@gmail.com>
2026-02-04 15:57:52 +0800

    add simple view blog screen

--- lib/screens/view_blog_screen.dart
+++ lib/screens/view_blog_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_2/providers/auth_providers.dart';
+import 'package:flutter_blog_2/providers/blog_providers.dart';
+import 'package:flutter_blog_2/utils.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class ViewBlogScreen extends StatefulWidget {
   const ViewBlogScreen({super.key, required this.id});
@@ -11,8 +15,8 @@ class ViewBlogScreen extends StatefulWidget {
   static Function(BuildContext context, String id) go = (context, id) =>
       context.go(callRouteName.replaceFirst(":id", id));
 
-  static Function(BuildContext context) push = (context) =>
-      context.push(routeName);
+  static Function(BuildContext context, String id) push = (context, id) =>
+      context.push(callRouteName.replaceFirst(":id", id));
 
   @override
   State<ViewBlogScreen> createState() {
@@ -23,6 +27,79 @@ class ViewBlogScreen extends StatefulWidget {
 class _ViewBlogScreenState extends State<ViewBlogScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text("data");
+    final blogState = context.watch<BlogProvider>().getBlogState;
+    final userState = context.watch<AuthProvider>().getState;
+
+    Widget content = Center(child: CircularProgressIndicator(strokeWidth: 2));
+    bool isAuthor = userState.user?.id == blogState.user?.id;
+
+    if (blogState.blog != null) {
+      content = ListView(
+        padding: EdgeInsets.all(12),
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    blogState.user?.imageUrl != null
+                        ? CircleAvatar(
+                            radius: 25,
+                            backgroundImage: NetworkImage(
+                              blogState.user!.imageUrl!,
+                            ),
+                          )
+                        : CircleAvatar(
+                            radius: 25,
+                            child: Text(
+                              blogState.user!.displayName!
+                                  .substring(0, 2)
+                                  .toUpperCase(),
+                            ),
+                          ),
+                    const SizedBox(width: 8),
+                    Text(
+                      toUpperCaseFirstChar(blogState.title!),
+                      maxLines: 1,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 8),
+              isAuthor
+                  ? IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.edit, size: 20),
+                    )
+                  : const SizedBox.shrink(),
+              isAuthor
+                  ? IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.delete, size: 20),
+                    )
+                  : const SizedBox.shrink(),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(vertical: 6),
+            child: const Divider(),
+          ),
+          Text(
+            toUpperCaseFirstChar(blogState.title!),
+            style: TextStyle(fontSize: 18),
+          ),
+          const SizedBox(height: 15),
+          Text(blogState.blog!),
+        ],
+      );
+    }
+    return Scaffold(
+      // key: scaffoldKey,
+      appBar: AppBar(title: const Text('View Blog')),
+      body: content,
+    );
   }
 }
