494f90821cb5123dab77dce994c2a84bb1fb9ec7
John Suyang <johnsuyang2118@gmail.com>
2026-02-08 07:45:13 +0800

    update comment model

--- lib/models/comment_model.dart
+++ lib/models/comment_model.dart
@@ -5,25 +5,33 @@ class CommentModel {
     required this.id,
     required this.comment,
     required this.user,
-    required this.imageUrls,
     required this.createdAt,
+    required this.blogId,
+    required this.parentId,
+    this.comments,
+    this.imageUrls,
     this.loading = false,
   });
   final String? id;
-  final String? comment;
+  final String comment;
   final String? createdAt;
-  final BlogUserModel? user;
+  final BlogUserModel user;
   final bool loading;
+  final String blogId;
+  final String parentId;
+  final List<CommentModel>? comments;
   final List<String>? imageUrls;
 
   factory CommentModel.initial() {
     return CommentModel(
       id: null,
       createdAt: null,
-      comment: null,
-      user: null,
+      comment: "",
+      user: BlogUserModel(displayName: "", id: "", imageUrl: null),
       loading: false,
       imageUrls: null,
+      blogId: "",
+      parentId: "",
     );
   }
 
@@ -31,18 +39,22 @@ class CommentModel {
     String? id,
     String? comment,
     String? blog,
+    String? blogId,
     BlogUserModel? user,
     String? createdAt,
     bool? loading,
     List<String>? imageUrls,
+    String? parentId,
   }) {
     return CommentModel(
+      blogId: blogId ?? this.blogId,
       imageUrls: imageUrls ?? this.imageUrls,
       id: id ?? this.id,
       createdAt: createdAt ?? this.createdAt,
       comment: comment ?? this.comment,
       user: user ?? this.user,
       loading: loading ?? this.loading,
+      parentId: parentId ?? this.parentId,
     );
   }
 }
