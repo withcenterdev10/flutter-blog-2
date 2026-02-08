f3cf255bffdb95ca56d4fee86e0608c2a216fd22
John Suyang <johnsuyang2118@gmail.com>
2026-02-08 11:55:19 +0800

    create blogUserModel json to dart model

--- lib/models/blog_user_model.dart
+++ lib/models/blog_user_model.dart
@@ -13,6 +13,24 @@ class BlogUserModel {
     return BlogUserModel(displayName: null, id: null, imageUrl: null);
   }
 
+  factory BlogUserModel.formJson(Map<String, dynamic> json) {
+    if (json case {
+      'id': final String id,
+      'image_url': final String imageUrl,
+      'display_name': final String displayName,
+    }) {
+      return BlogUserModel(
+        id: id,
+        imageUrl: imageUrl,
+        displayName: displayName,
+      );
+    } else {
+      throw const FormatException(
+        'Unexpected JSON format (BlogUserModel.formJson)',
+      );
+    }
+  }
+
   BlogUserModel copyWith({String? id, String? imageUrl, String? displayName}) {
     return BlogUserModel(
       displayName: displayName ?? this.displayName,
