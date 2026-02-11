36fb7c95f508e09af7a05ee4bf3cebdbb290f3c8
John Suyang <johnsuyang2118@gmail.com>
2026-02-12 05:30:46 +0800

    remove edit and delete icon

--- lib/widgets/blog/blog.dart
+++ lib/widgets/blog/blog.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog_2/models/auth_model.dart';
 import 'package:flutter_blog_2/models/blog_model.dart';
-import 'package:flutter_blog_2/providers/auth_providers.dart';
 import 'package:flutter_blog_2/providers/blog_providers.dart';
-import 'package:flutter_blog_2/screens/blogs_screen.dart';
-import 'package:flutter_blog_2/screens/edit_blog_screen.dart';
 import 'package:flutter_blog_2/screens/view_blog_screen.dart';
 import 'package:flutter_blog_2/utils.dart';
 import 'package:flutter_blog_2/widgets/avatar.dart';
@@ -24,86 +20,8 @@ class Blog extends StatelessWidget {
     }
   }
 
-  void handleDelete(
-    BuildContext context,
-    BlogModel blogState,
-    AuthModel userState,
-  ) async {
-    final isAuthor = userState.user?.id == blogState.user?.id;
-    if (isAuthor) {
-      String message = "";
-      try {
-        await context.read<BlogProvider>().deleteBlog(
-          id: blogState.id,
-          userId: userState.user!.id,
-        );
-        message = "Blog deleted";
-      } catch (error) {
-        message = "Delete blog failed";
-        debugPrint(error.toString());
-      } finally {
-        if (context.mounted) {
-          BlogsScreen.go(context);
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text(message)));
-        }
-      }
-    } else {
-      if (context.mounted) {
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text("Action not allowed")));
-      }
-    }
-  }
-
-  Future<void> showDeleteDialog(
-    BuildContext context,
-    BlogModel blogState,
-    AuthModel authState,
-  ) async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Delete blog'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('Are you sure you want to delete this blog?'),
-                SizedBox(height: 10),
-                Text(blogState.title!),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: const Text('Approve'),
-              onPressed: () {
-                handleDelete(context, blogState, authState);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    bool isDesktop = MediaQuery.of(context).size.width >= 900;
-    final blogState = context.watch<BlogProvider>().getBlogState;
-    final authState = context.watch<AuthProvider>().getState;
-    final isAuthor = authState.user?.id == blog.user?.id;
-
     return Card(
       margin: EdgeInsets.all(12),
       clipBehavior: Clip.hardEdge,
@@ -134,21 +52,6 @@ class Blog extends StatelessWidget {
                           softWrap: false,
                           overflow: TextOverflow.ellipsis,
                         ),
-                        if (isDesktop && isAuthor) ...<Widget>[
-                          const Spacer(),
-                          IconButton(
-                            onPressed: () {
-                              EditBlogScreen.push(context);
-                            },
-                            icon: const Icon(Icons.edit, size: 20),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              showDeleteDialog(context, blogState, authState);
-                            },
-                            icon: const Icon(Icons.delete, size: 20),
-                          ),
-                        ],
                       ],
                     ),
                   ),
